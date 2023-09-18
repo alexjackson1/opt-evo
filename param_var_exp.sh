bb47665dc898c83b9259c58efade4862c67a3925
Alex Jackson <mail@alexjackson.uk>
2023-09-18 04:14:00 +0100

    Fix job bash

--- param_var_exp.sh
+++ param_var_exp.sh
@@ -15,21 +15,27 @@ task_id=$SLURM_ARRAY_TASK_ID
 echo "SLURM_ARRAY_TASK_ID: $task_id"
 
 # Load modules
-# module load $anaconda_module
-# source /users/${USER}/.bashrc
-# source activate $conda_prefix
+module load $anaconda_module
+source /users/${USER}/.bashrc
+source activate $conda_prefix
 
-$seed_values=(1 2 3 4 5) # 5
-$noise_p_values=(0.001 0.002 0.005 0.01) # 4
-$tournament_size_values=(5 10 20) # 3
-$batch_size_values=(128 256) # 2
-$elites_values=(1 2 5 10) # 4
+$seed_values=$(( (1 2 3 4 5) )) # 5
+$noise_p_values=$(( (0.001 0.002 0.005 0.01) )) # 4
+$tournament_size_values=$(( (5 10 20) )) # 3
+$batch_size_values=$(( (128 256) )) # 2
+$elites_values=$(( (1 2 5 10) )) # 4
 
-$seed=${seed_values[$((task_id % 5))]}
-$noise_p=${noise_p_values[$(((task_id / 5) % 4))]}
-$tournament_size=${tournament_size_values[$(((task_id / 20) % 3))]}
-$batch_size=${batch_size_values[$(((task_id / 60) % 2))]}
-$elites=${elites_values[$(((task_id / 120) % 4))]}
+$seed_index=$(( $task_id % 5 ))
+$noise_p_index=$(( ($task_id / 5) % 4 ))
+$tournament_size_index=$(( ($task_id / 20) % 3 ))
+$batch_size_index=$(( ($task_id / 60) % 2 ))
+$elites_index=$(( ($task_id / 120) % 4 ))
+
+$seed=${seed_values[$seed_index]}
+$noise_p=${noise_p_values[$noise_p_index]}
+$tournament_size=${tournament_size_values[$tournament_size_index]}
+$batch_size=${batch_size_values[$batch_size_index]}
+$elites=${elites_values[$elites_index]}
 
 echo "seed: $seed"
 echo "noise_p: $noise_p"
