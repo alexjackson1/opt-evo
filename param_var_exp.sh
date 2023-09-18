17f702cb0abf42e17622ebf3d0559808af42b065
Alex Jackson <mail@alexjackson.uk>
2023-09-18 05:43:07 +0100

    Fix experiment file

--- param_var_exp.sh
+++ param_var_exp.sh
@@ -6,7 +6,7 @@
 #SBATCH --gres=gpu
 
 anaconda_module="anaconda3/2022.10-gcc-10.3.0"
-conda_prefix="/scratch/users/${USER}/conda_envs/jax"
+conda_prefix="/scratch/users/${USER}/conda_envs/ne_simp"
 script_path="/users/${USER}/optevo/main.py"
 out_dir="/scratch/users/${USER}/ne_results/param_var/"
 
@@ -19,23 +19,23 @@ module load $anaconda_module
 source /users/${USER}/.bashrc
 source activate $conda_prefix
 
-$seed_values=$(( (1 2 3 4 5) )) # 5
-$noise_p_values=$(( (0.001 0.002 0.005 0.01) )) # 4
-$tournament_size_values=$(( (5 10 20) )) # 3
-$batch_size_values=$(( (128 256) )) # 2
-$elites_values=$(( (1 2 5 10) )) # 4
+seed_values=(1 2 3 4 5)
+noise_p_values=(0.001 0.002 0.005 0.01)
+tournament_size_values=(5 10 20)
+batch_size_values=(128 256)
+elites_values=(1 2 5 10)
 
-$seed_index=$(( $task_id % 5 ))
-$noise_p_index=$(( ($task_id / 5) % 4 ))
-$tournament_size_index=$(( ($task_id / 20) % 3 ))
-$batch_size_index=$(( ($task_id / 60) % 2 ))
-$elites_index=$(( ($task_id / 120) % 4 ))
+seed_index=$(( $task_id % 5 ))
+noise_p_index=$(( ($task_id / 5) % 4 ))
+tournament_size_index=$(( ($task_id / 20) % 3 ))
+batch_size_index=$(( ($task_id / 60) % 2 ))
+elites_index=$(( ($task_id / 120) % 4 ))
 
-$seed=${seed_values[$seed_index]}
-$noise_p=${noise_p_values[$noise_p_index]}
-$tournament_size=${tournament_size_values[$tournament_size_index]}
-$batch_size=${batch_size_values[$batch_size_index]}
-$elites=${elites_values[$elites_index]}
+seed=${seed_values[$seed_index]}
+noise_p=${noise_p_values[$noise_p_index]}
+tournament_size=${tournament_size_values[$tournament_size_index]}
+batch_size=${batch_size_values[$batch_size_index]}
+elites=${elites_values[$elites_index]}
 
 echo "seed: $seed"
 echo "noise_p: $noise_p"
@@ -48,7 +48,7 @@ Run the script
 python $script_path \
   --seed=$seed \
   --out_dir=$HOME/optevo/results \
-  --data_dir=$HOME/optevo/data \
+  --data_dir=$HOME/optevo/data_2 \
   --arch="ff" \
   --max_gen=5000 \
   --pop_size=50 \
